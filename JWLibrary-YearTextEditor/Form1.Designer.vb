eea98f62f12807aadaa01d2f132d004ddda43f77
Miaosi001 <46404000+Miaosi001@users.noreply.github.com>
2020-01-06 23:42:26 +0100

    Add files via upload

---
+++ JWLibrary-YearTextEditor/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(239, 110)
+        Me.TextBox1.TabIndex = 0
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(258, 15)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(123, 17)
+        Me.CheckBox1.TabIndex = 3
+        Me.CheckBox1.Text = "Use default year text"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(258, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Font size:"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.DecimalPlaces = 2
+        Me.NumericUpDown1.Increment = New Decimal(New Integer() {2, 0, 0, 65536})
+        Me.NumericUpDown1.Location = New System.Drawing.Point(259, 74)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(185, 20)
+        Me.NumericUpDown1.TabIndex = 5
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(258, 100)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(185, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(258, 38)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(47, 17)
+        Me.CheckBox2.TabIndex = 7
+        Me.CheckBox2.Text = "Bold"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(10, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(429, 14)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "*For each change it is necessary to restart the secondary monitor from the JW Lib" & _
+            "rary application"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(459, 143)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "JWLibrary-YearTextEditor"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ JWLibrary-YearTextEditor/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System
+Imports System.IO
+Public Class Form1
+    Dim JsonPath As String = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) & "\Packages"
+    Dim JsonText As String
+    Const JsonYearText As String = "e.markup"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ChangeLanguage()
+        If IO.Directory.Exists(JsonPath) Then
+            For Each Dir As String In Directory.GetDirectories(JsonPath)
+                If LCase(Dir).Contains("watchtower") Then
+                    JsonPath = Dir & "\LocalState\www\webapp\YearTextDisplay.bundle.js"
+                    Exit For
+                End If
+            Next
+        End If
+        If Not System.IO.File.Exists(JsonPath) Then
+            MsgBox(Strings.Error1Text, MsgBoxStyle.Critical, Strings.Error1Text)
+            Return
+        End If
+        Dim myStream = System.IO.File.OpenText(JsonPath)
+        JsonText = myStream.ReadToEnd()
+        myStream.Close()
+
+        Dim JsonPart1 = Split(JsonText, "\r\n    font-size: ")(0) & "\r\n    font-size: "
+        Dim JsonPart2 = "vw;\r\n    text-align: center;" & Split(Split(JsonText, "\r\n    font-size: ")(1), "vw;\r\n    text-align: center;")(1)
+        Dim ActFontSize = JsonText.Replace(JsonPart1, "") : ActFontSize = ActFontSize.Replace(JsonPart2, "")
+        NumericUpDown1.Value = Val(ActFontSize)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim PreJson = JsonChangeText(JsonText, TextBox1.Text)
+        Dim NewJson = JsonChangeFontSize(PreJson)
+        Dim sWriter As New IO.StreamWriter(JsonPath, False)
+        sWriter.Write(NewJson)
+        sWriter.Close()
+    End Sub
+
+    Function JsonChangeText(ByVal Json As String, ByVal Text As String) As String
+        Dim VarS As String = "},dangerouslySetInnerHTML:{__html:"
+        Dim JsonPart1 As String = Split(Json, VarS)(0) & "},dangerouslySetInnerHTML:{__html:"
+        Dim JsonPart2 As String = "}}))}" & Split(Split(Json, VarS)(1), "}}))}")(1)
+        If CheckBox1.Checked Then
+            JsonChangeText = JsonPart1 & JsonYearText & JsonPart2
+        Else
+            JsonChangeText = JsonPart1 & """" & CheckText(Text) & """"
+            If CheckBox2.Checked Then JsonChangeText &= ".bold()"
+            JsonChangeText &= JsonPart2
+        End If
+    End Function
+
+    Function JsonChangeFontSize(ByVal Json As String) As String
+        Dim JsonPart1 = Split(Json, "\r\n    font-size: ")(0) & "\r\n    font-size: "
+        Dim JsonPart2 = "vw;\r\n    text-align: center;" & Split(Split(Json, "\r\n    font-size: ")(1), "vw;\r\n    text-align: center;")(1)
+        JsonChangeFontSize = JsonPart1 & FormatNumber(NumericUpDown1.Value, 2).Replace(",", ".") & JsonPart2
+    End Function
+
+    Function CheckText(ByVal Text As String) As String
+        If Text.Contains("""") Then Text = Text.Replace("""", "")
+        If Text.Contains(vbCr) Then Text = Text.Replace(vbCr, " <br> ")
+        Return Text
+    End Function
+
+    Sub ChangeLanguage()
+        Dim UserCulture = System.Globalization.CultureInfo.CurrentCulture
+        If LCase(UserCulture.ToString).Contains("it") Then
+            Strings.Error1Text = "Il file non esiste!"
+            Strings.Error1Title = "Errore"
+            Strings.CheckBox1Text = "Usa la scrittura dell'anno predefinita"
+            Strings.CheckBox2Text = "Grassetto"
+            Strings.Label1Text = "Dimensione font:"
+            Strings.Button1Text = "Salva"
+            Strings.InfoText = "*Per ogni cambiamento è necessario riavviare lo schermo secondario dall'applicazione JW Library"
+        End If
+        CheckBox1.Text = Strings.CheckBox1Text
+        CheckBox2.Text = Strings.CheckBox2Text
+        Label1.Text = Strings.Label1Text
+        Button1.Text = Strings.Button1Text
+        Label2.Text = Strings.InfoText
+    End Sub
+
+    Structure Strings
+        Shared Error1Text As String = "The file does not exist"
+        Shared Error1Title As String = "Error"
+        Shared CheckBox1Text As String = "Use default year text"
+        Shared CheckBox2Text As String = "Bold"
+        Shared Label1Text As String = "Font size:"
+        Shared Button1Text As String = "Save"
+        Shared InfoText As String = "*For each change it is necessary to restart the secondary monitor from the JW Library application"
+    End Structure
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        TextBox1.Enabled = Not CheckBox1.Checked
+        CheckBox2.Enabled = Not CheckBox1.Checked
+        TextBox1.Text = ""
+    End Sub
+End Class
